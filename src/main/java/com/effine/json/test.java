b8410524c30ae0975bfc02543f5b8cf1fc8f0e6f
verphen <verphen@163.com>
2014-12-17 00:03:30 +0800

    update dir for maven

---
+++ src/main/java/com/effine/json/test.java
@@ -0,0 +1,10 @@
+package main.java.com.effine.json;
+
+
+
+public class test {
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException {
+//		HttpSession session =  HttpSession.class.newInstance();
+		
+	}
+}
